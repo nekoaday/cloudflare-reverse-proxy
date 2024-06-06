2920d9e1f8b16c02211a97cd93441674d7db2d71
nekoaday <118155860+nekoaday@users.noreply.github.com>
2024-06-06 23:18:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,43 +1,202 @@
+/*
+name: cloudflare安全代理
+author: eray
+version: v1.0
+blog: blog.eray.cc
+email: admin@eray.cc
+
+desp: 本项目可以给cf代理设置一个密码，使代理更安全，调用更方便
+
+config set:
+需要替换：
+
+密码验证路径（可选）：
+/your-pwd-verify-path
+
+越权Api代理前缀路径[用于代理openai等api]
+（可以直接注释删除，不使用）:
+/your-safe-api-prefix/
+
+访问密码（推荐设置复杂一些）：
+your-access-pwd
+
+*/
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+	event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-  const url = new URL(request.url);
 
-  // 新增：检查用户是否直接访问代理地址
-  if (url.pathname === '/' || url.pathname === '/proxy/') {
-    return createLandingPage();
-  }
+	const url = new URL(request.url);
 
-  const actualUrlStr = url.pathname.replace("/proxy/","") + url.search + url.hash
+	// Check if some api direct browse, the path should be hard to guess, u can comment it out if you don't need it
+	if (url.pathname.includes('/your-safe-api-prefix/')) {
+		return handleApiRequest(request);
+	}
 
-  const actualUrl = new URL(actualUrlStr)
+	// Check if the request is for password verification
+	if (url.pathname === '/your-pwd-verify-path' && request.method === 'POST') {
+		const password = await request.text();
+		if (verifyPassword(password)) {
+			return new Response('success', {
+				headers: {
+					'Content-Type': 'text/plain'
+				},
+			});
+		} else {
+			return new Response('failure', {
+				headers: {
+					'Content-Type': 'text/plain'
+				},
+			});
+		}
+	}
 
-  const modifiedRequest = new Request(actualUrl, {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: 'follow'
-  });
+	// Check if password cookie exists
+	const savedPassword = request.headers.get('Cookie');
 
-  const response = await fetch(modifiedRequest);
-  const modifiedResponse = new Response(response.body, response);
+	if (!savedPassword) {
+		return createPasswordPage();
+	}
 
-  // 添加允许跨域访问的响应头
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+	//if exists cookie but is not pwd
+	if (savedPassword) {
+		const parsedCookies = parseCookies(savedPassword);
+		const passwordValue = parsedCookies['password'];
+		if (!verifyPassword(passwordValue)) {
+			return createPasswordPage();
+		}
+	}
 
-  return modifiedResponse;
+	// Check if accessing path
+	if (url.pathname === '/' || url.pathname === '/proxy/' || url.pathname === '/proxy') {  
+		return createLandingPage();
+	}
+
+	// If password cookie exists and accessing other paths, handle proxying
+	return handleProxyRequest(request);
 }
 
-// 新增：创建引导页面
-function createLandingPage() {
-  const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <style>
-  body {
+async function handleProxyRequest(request) {
+	const url = new URL(request.url);
+
+	// Use fixUrl function to ensure valid URL protocol
+	const actualUrlStr = fixUrl(url.pathname.replace("/proxy/", "")) + url.search + url.hash;
+	const actualUrl = new URL(actualUrlStr);
+
+  // Copy headers from the original request, but remove any CF headers  
+  let newHeaders = new Headers();
+  for (let pair of request.headers.entries()) {
+    if (!pair[0].startsWith('cf-')) {
+      newHeaders.append(pair[0], pair[1]);
+    }
+  }
+	const modifiedRequest = new Request(actualUrl, {
+		headers: newHeaders,
+		method: request.method,
+		body: request.body,
+		redirect: 'manual'
+	});
+
+	const response = await fetch(modifiedRequest);
+  let modifiedResponse;
+  let body = response.body;
+
+  // Handle redirects
+  if ([301, 302, 303, 307, 308].includes(response.status)) {
+    const location = new URL(response.headers.get('location'));
+    const modifiedLocation = "/proxy/" + location.toString();
+    modifiedResponse = new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText
+    });
+    modifiedResponse.headers.set('Location', modifiedLocation);
+  } else {
+    if (response.headers.get("Content-Type") && response.headers.get("Content-Type").includes("text/html")) {
+      const originalText = await response.text();
+      const regex = new RegExp('((href|src|action)=["\'])/(?!/)', 'g');
+      let modifiedText = originalText.replace(regex, `$1${url.protocol}//${url.host}/proxy/${new URL(actualUrlStr).origin + "/"}`);
+      const regex2 = new RegExp('((href|src|action)=["\'])https\://github.com', 'g');
+      modifiedText = originalText.replace(regex2, `$1${url.protocol}//${url.host}/proxy/https://github.com`);
+      //我费了，谁会谁改吧，下面加上后加不出css，不加的话完整路径，search的只有？的也不行
+      //const regex2 = new RegExp('((href|src|action)=["\'])http', 'g');
+      //modifiedText = originalText.replace(regex2, `$1${url.protocol}//${url.host}/proxy/http`);
+      body = modifiedText;
+    }
+
+    modifiedResponse = new Response(body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers
+    });
+  }
+
+	// Add headers for CORS
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+    modifiedResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    modifiedResponse.headers.set('Access-Control-Allow-Headers', '*');
+	return modifiedResponse;
+}
+
+async function handleApiRequest(request) {
+	const url = new URL(request.url);
+	// Use fixUrl function to ensure valid URL protocol
+	const actualUrlStr = fixUrl(url.pathname.replace("/your-safe-api-prefix/", "")) + url.search + url.hash;
+	const actualUrl = new URL(actualUrlStr);
+
+	const modifiedRequest = new Request(actualUrl, {
+		headers: request.headers,
+		method: request.method,
+		body: request.body,
+		redirect: 'follow'
+	});
+
+	const response = await fetch(modifiedRequest);
+	const modifiedResponse = new Response(response.body, response);
+
+	// Add headers for CORS
+	modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+
+	return modifiedResponse;
+}
+
+function fixUrl(url) {
+	if (url.includes("github")) {//this is for github    
+    if (url.includes("://")) {
+      return url;
+    } else if (url.includes(':/')) {
+      return url.replace(':/', '://');
+    } else {
+      return "http://" + url;
+    }
+  }
+  else
+      return "https://github.com"+url;
+}
+
+// parse cookie
+function parseCookies(cookieString) {
+	return cookieString.split(';')
+		.reduce((cookies, cookie) => {
+			const [name, value] = cookie.trim()
+				.split('=');
+			cookies[name] = value;
+			return cookies;
+		}, {});
+}
+
+// Create the password verification page
+function createPasswordPage() {
+	const html = `
+    <!DOCTYPE html>
+    <html lang="zh-CN">
+    <head>
+      <meta charset="UTF-8">
+      <title>密码验证</title>
+    </head>
+    <style>
+    body {
     background-color: #fbfbfb;
     font-family: Arial, sans-serif;
   }
@@ -86,31 +245,67 @@ function createLandingPage() {
   button:hover {
     background-color: #039BE5;
   }
-</style>
-    <meta charset="UTF-8">
-    <title>代理服务器</title>
-  </head>
+    </style>
+    <body>
+      <h1>密码验证</h1>
+      <form id="password-form">
+        <input type="password" id="password" name="password" placeholder="请输入访问密码" required />
+        <button type="submit">验证密码</button>
+      </form>
+      <script>
+        const form = document.getElementById('password-form');
+        form.addEventListener('submit', event => {
+          event.preventDefault();
+          const input = document.getElementById('password');
+          const password = input.value;
+
+          // Send password for verification
+          fetch('/your-pwd-verify-path', { method: 'POST', body: password })
+            .then(response => response.text())
+            .then(result => {
+              if (result === 'success') {
+                // Password verification successful, set cookie, and redirect to the landing page
+                document.cookie = 'password=' + password + '; expires=' + new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString();
+                location.href = '/proxy/';
+              } else {
+                alert('密码验证失败！');
+              }
+            });
+        });
+      </script>
+    </body>
+    </html>
+  `;
+
+	return new Response(html, {
+		headers: {
+			'Content-Type': 'text/html'
+		}
+	});
+}
+
+// Create the landing page
+function createLandingPage() {
+	const html = `
+  <!DOCTYPE html>
   <body>
-    <h1>输入您想访问的网址</h1>
-    <form id="proxy-form">
-      <input type="text" id="url" name="url" placeholder="https://example.com" required />
-      <button type="submit">访问</button>
-    </form>
     <script>
-      const form = document.getElementById('proxy-form');
-      form.addEventListener('submit', event => {
-        event.preventDefault();
-        const input = document.getElementById('url');
-        const actualUrl = input.value;
-        const proxyUrl = '/proxy/' + actualUrl;
-        location.href = proxyUrl;
-      });
+      const proxyUrl = '/proxy/github.com';
+      location.href = proxyUrl;
     </script>
   </body>
   </html>
   `;
-  
-  return new Response(html, {
-    headers: { 'Content-Type': 'text/html' }
-  });
+
+	return new Response(html, {
+		headers: {
+			'Content-Type': 'text/html'
+		}
+	});
+}
+
+// Verify the password function
+function verifyPassword(savedPassword) {
+	// Replace this logic with your actual password verification
+	return savedPassword === "your-access-pwd";
 }
